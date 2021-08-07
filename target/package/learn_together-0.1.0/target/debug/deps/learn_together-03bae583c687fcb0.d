5a7685e26f6a2b7ff516987f128e2b5988480988
wandalen <wandalen.me@gmail.com>
2021-08-07 21:04:22 +0300

    readme

--- target/package/learn_together-0.1.0/target/debug/deps/learn_together-03bae583c687fcb0.d
+++ target/package/learn_together-0.1.0/target/debug/deps/learn_together-03bae583c687fcb0.d
@@ -1,7 +1,7 @@
-/pro/rust/module/LearnRustTogether/target/debug/deps/learn_together-03bae583c687fcb0.rmeta: src/lib.rs
+/pro/rust/module/LearnRustTogether/target/package/learn_together-0.1.0/target/debug/deps/learn_together-03bae583c687fcb0.rmeta: src/lib.rs
 
-/pro/rust/module/LearnRustTogether/target/debug/deps/liblearn_together-03bae583c687fcb0.rlib: src/lib.rs
+/pro/rust/module/LearnRustTogether/target/package/learn_together-0.1.0/target/debug/deps/liblearn_together-03bae583c687fcb0.rlib: src/lib.rs
 
-/pro/rust/module/LearnRustTogether/target/debug/deps/learn_together-03bae583c687fcb0.d: src/lib.rs
+/pro/rust/module/LearnRustTogether/target/package/learn_together-0.1.0/target/debug/deps/learn_together-03bae583c687fcb0.d: src/lib.rs
 
 src/lib.rs:
